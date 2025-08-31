e056b2c332cb46457e82bad341abbd052d9cf2a7
redsamurairuka <redsamurairuka@gmail.com>
2025-08-31 11:29:25 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -12,7 +12,7 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'xopahe1269@amcret.com', // Replace with your Mega email
+    email: 'pijidag940@cavoyar.com', // Replace with your Mega email
     password: '120363419684343607@newsletter', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
